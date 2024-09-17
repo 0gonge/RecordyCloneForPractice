2838668c2a2d8cac5beb979b6005377ddadd316e
0gonge <Challengef0802@gmail.com>
2024-09-18 04:52:53 +0900

    docs: gitignore적용 및 초기 커밋

---
+++ RecordyClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RecordyClone
+//
+//  Created by 송여경 on 9/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
